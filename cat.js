c2cb7bd3817c9b38d9cbcf5ebdecbba2ea2e6769
unknown <ttddcc119@naver.com>
2024-02-12 20:37:29 +0900

    JSDOC 추가

--- cat.js
+++ cat.js
@@ -3,10 +3,17 @@ export default class Cat {
     this.body = document.querySelector('body');
     this.maxHeight = document.querySelector('body').clientHeight;
     this.imgSize = 50;
+    this.caching = {};
     this.createCat(locationX);
     this.render();
   }
 
+  /**
+   * This function creates a node that moves up and down within the viewport.
+   * The node's class and locationX are randomly choosen.
+   * All location infromation is cached.
+   * @param {Number} locationX - Indicates the horizontal X coordinate where the node will be placed.
+   */
   createCat(locationX) {
     const { maxHeight, imgSize, caching } = this;
     const movingState = Math.random() > 0.5 ? 'up' : 'down';
@@ -17,7 +24,10 @@ export default class Cat {
     node.className = `cat ${movingState}`;
     node.style.cssText = `left: ${locationX}px; top: ${locationY}px; transform : translateY(0px);`;
 
-    this.locationCached = locationY;
+    caching.curTop = locationY;
+    caching.curTranslateY = 0;
+    caching.curLocation = locationY - 0;
+
     this.node = node;
   }
 
@@ -26,38 +36,71 @@ export default class Cat {
     body.appendChild(node);
   }
 
+  /**
+   * Parsing the style.transform text to calculate the translateY value
+   * @constant {number} start - index of character follwing '('
+   * @constant {number} end - index of character preceding 'px'
+   * @returns {number} - The current translateY value of the node.
+   */
   translateParsing = () => {
     const { node } = this;
     const translateText = node.style.transform;
-    const pxLocation = translateText.indexOf('px');
-
-    return translateText.slice(11, pxLocation);
+    const start = translateText.indexOf('(') + 1;
+    const end = translateText.indexOf('px');
+    return parseInt(translateText.slice(start, end));
   };
 
+  /**
+   *
+   * @param {boolean} isCaching - Indicates whether to use cached location values
+   * @returns {Object} - infromation of the node.
+   * @property {number} curTop - The current top position of the node.
+   * @property {number} curTranslateY - The current translateY value of the node.
+   * @property {number} curLocation - The current calculated location of the node
+   *
+   */
   getLocation = (isCaching) => {
-    const { node, locationCached } = this;
+    const { node, caching } = this;
     const { translateParsing } = this;
     const amountTranslate = translateParsing();
 
-    if (isCaching) return locationCached;
-    return node.offsetTop + amountTranslate;
+    if (isCaching) return caching;
+    return {
+      curTop: node.offsetTop,
+      curTranslateY: amountTranslate,
+      curLocation: node.offsetTop - amountTranslate,
+    };
   };
-
-  correctLocation = (isGoingUp, isCaching) => {
-    const { maxHeight, imgSize } = this;
+  /**
+   * Calculate the offset to going next step depending on whether it is cached or not
+   * offset is the distance to move depending on the current location
+   * if the distnace after moving by offset is beyond the viewport , the offset is adjusted to the
+   * distance from the current position to viewport.
+   * @param {boolean} isCaching - Indicates whether caching for animation if enabled.
+   * @returns {number} - Indicates the distance the node will move next.
+   */
+  calcaulateOffset = (isCaching) => {
+    const { node, maxHeight, imgSize } = this;
     const { getLocation } = this;
-    const distance = isGoingUp ? -3 : 3;
-    const { curTop } = getLocation(isCaching);
-    let nextLocation = curTop + distance;
+    const { curLocation } = getLocation(isCaching);
+    const isGoingUp = node.classList.contains('up');
+    const offset = isGoingUp ? -3 : 3;
+    const nextLocation = curLocation + offset;
 
-    if (isGoingUp) {
-      nextLocation = Math.max(nextLocation, 0);
-    } else {
-      nextLocation = Math.min(nextLocation, maxHeight - imgSize);
-    }
-    return nextLocation;
+    if (isGoingUp && nextLocation <= 0) return -curLocation;
+    if (!isGoingUp && nextLocation >= maxHeight - imgSize)
+      return maxHeight - curLocation;
+
+    return offset;
   };
 
+  /**
+   * function to change the node's class based on the next direction to move,
+   * wheter the next direction to go is up or down
+   * this is depending on nextLocation is beyond the viewport
+   * @param {boolean} isGoingUp - Indicates the node's class contains up
+   * @param {boolean} nextLocation - Indicates the location where the next node will be placed
+   */
   changeState = (isGoingUp, nextLocation) => {
     const { node, maxHeight, imgSize } = this;
     if (nextLocation !== 0 && nextLocation !== maxHeight - imgSize) return;
@@ -71,13 +114,13 @@ export default class Cat {
     }
   };
 
-  updateCache = (nextLocation) => {
-    const { caching } = this;
-    caching.curTop = nextLocation;
+  updateCache = (newData) => {
+    this.caching = { ...this.caching, ...newData };
   };
 
   move(optimizeState) {
     const { isCaching, isTranslate } = optimizeState;
-    this.getLocation(isCaching);
+    const { calcaulateOffset, changeState, updateCache } = this;
+    const offset = calcaulateOffset(isCaching);
   }
 }
